8631b8db50e01ded4a8a3e6cc1c9543cbf871b9d
Toby Stayner <toby@swengineer.dev>
2023-06-13 09:07:53 +1200

    Add various positions for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css';
 import Board from './Board';
 
 const STARTING_POSITION =
   'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const CASTLING_POSITION = 'r3k2r/pppp1ppp/8/8/8/8/PPPP1PPP/R3K2R w KQkq - 0 1';
+const EN_PASSANT_POSITION = '3k4/4p1p1/8/5P1P/1p1p4/8/P1P5/3K4 w - - 0 1';
+const PROMOTION_POSITION = '8/1K4P1/8/8/8/8/1k4p1/8 w - - 0 1';
+const CHECKMATE_POSITION = '6k1/4Rppp/8/8/8/8/5PPP/6K1 w - - 0 1';
+const STALEMATE_POSITION = '8/8/8/8/8/3pk3/8/4K3 b - - 0 1';
 
 export const RANKS = 8;
 export const FILES = 8;
